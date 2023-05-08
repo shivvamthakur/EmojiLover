116ed229617e07b1e7b8f734a9bd8c9518283145
Shivam Thakur <shivvamthakurr@gmail.com>
2023-05-09 00:35:54 +0530

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Shivam Thakur on 07/05/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmojiLover/SignUp.swift
@@ -0,0 +1,20 @@
+//
+//  SignUp.swift
+//  EmojiLover
+//
+//  Created by Shivam Thakur on 08/05/23.
+//
+
+import SwiftUI
+
+struct SignUp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUp_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUp()
+    }
+}
